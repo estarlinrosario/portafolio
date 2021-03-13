e579d662ad4817de5b380bca27e7301b532d6246
estarlinrosario <estarlinrosario13@hotmail.com>
2021-03-12 22:54:18 -0400

    Mejora al projecto

--- source/js/main.js
+++ source/js/main.js
@@ -1,42 +1,39 @@
 window.onload = function() {
     var contactme = document.getElementById('contactme');
     let verMas = document.getElementsByName('readmore');
-    var msj = '849-268-1558';
-
-
-
 
     verMas.forEach(function(vermas) {
         vermas.addEventListener('click', function() {
             mensajeError();
+
         });
     })
 
-
-
     contactme.addEventListener('click', function(msj) {
+
         mensaje(msj);
+
     });
 
-
-
-
     function mensaje() {
+        var msj = '849-268-1558';
+
         swal({
             title: 'Phone Number',
             text: msj,
             icon: 'success'
         })
+
     }
 
     function mensajeError() {
-        msj = '';
-        msj = 'Working in Progress';
+        var msjerror = 'Working in Progress';
         swal({
             title: 'Warning',
-            text: msj,
+            text: msjerror,
             icon: 'warning'
         })
+
     }
 
 }
